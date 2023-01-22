378fb1f4d8fcf88d40c43de5e140a756094f8579
Renato Lima <natenho@gmail.com>
2023-01-22 16:18:27 -0300

    Improve attachment error handling

--- migration/attachments.go
+++ migration/attachments.go
@@ -1,6 +1,8 @@
 package migration
 
 import (
+	"fmt"
+	"net/http"
 	"sync"
 
 	"github.com/natenho/go-jira"
@@ -26,13 +28,29 @@ func (s *migrator) migrateAttachments(sourceIssue *jira.Issue, targetIssue *jira
 }
 
 func (s *migrator) migrateAttachment(attachment *jira.Attachment, targetIssueID string) (err error) {
+	if attachment == nil {
+		return errors.New("Invalid attachment")
+	}
+
 	response, err := s.sourceClient.Issue.DownloadAttachment(attachment.ID)
 	if err != nil {
 		return errors.Errorf("Could not download attachment %s %s", attachment.Filename, err)
 	}
-	defer response.Body.Close()
 
-	_, response, err = s.targetClient.Issue.PostAttachment(targetIssueID, response.Body, attachment.Filename)
+	var contentLength int64
+	if response != nil {
+		defer response.Body.Close()
+		contentLength = response.ContentLength
+	}
 
-	return parseResponseError("migrateAttachment", response, err)
+	_, postResponse, err := s.targetClient.Issue.PostAttachment(targetIssueID, response.Body, attachment.Filename)
+	if postResponse != nil {
+		defer postResponse.Body.Close()
+	}
+
+	if postResponse.StatusCode == http.StatusRequestEntityTooLarge {
+		err = errors.Wrapf(err, "Review upload limits for target account: Refer to https://support.atlassian.com/jira-cloud-administration/docs/configure-file-attachments/")
+	}
+
+	return parseResponseError(fmt.Sprintf("migrateAttachment(%s, %d bytes)", attachment.Filename, contentLength), postResponse, err)
 }
