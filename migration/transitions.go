326087ddf355cc33582f928e5022078795ff0af6
Renato Lima <natenho@gmail.com>
2023-02-06 23:52:30 -0300

    Fix channel deadlock on status migration error

--- migration/transitions.go
+++ migration/transitions.go
@@ -2,28 +2,38 @@ package migration
 
 import (
 	"strings"
+	"sync"
 
 	"github.com/natenho/go-jira"
 )
 
 func (s *migrator) migrateStatus(sourceIssue *jira.Issue, targetIssue *jira.Issue) chan error {
+	wg := &sync.WaitGroup{}
+
 	errChan := make(chan error, len(sourceIssue.Fields.Attachments))
+	defer close(errChan)
 
 	targetTransitions, response, err := s.targetClient.Issue.GetTransitions(targetIssue.Key)
 	if err != nil {
-		errChan <- parseResponseError("GetTransitions", response, err)
+		go func() {
+			errChan <- parseResponseError("GetTransitions", response, err)
+		}()
+		return errChan
 	}
 
 	for _, targetTransition := range targetTransitions {
 		if strings.EqualFold(targetTransition.To.StatusCategory.Name, sourceIssue.Fields.Status.StatusCategory.Name) {
 			if response, err := s.targetClient.Issue.DoTransition(targetIssue.Key, targetTransition.ID); err != nil {
-				errChan <- parseResponseError("DoTransition", response, err)
+				wg.Add(1)
+				go func() {
+					errChan <- parseResponseError("DoTransition", response, err)
+					wg.Done()
+				}()
 			}
-
 			break
 		}
 	}
 
-	close(errChan)
+	wg.Wait()
 	return errChan
 }
