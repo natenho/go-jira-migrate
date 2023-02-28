e9b0b3a175a5e6b541558ee5002cac38285b3bf5
Renato Lima <natenho@gmail.com>
2023-02-27 23:35:31 -0300

    Fix channel deadlock

--- migration/comments.go
+++ migration/comments.go
@@ -2,20 +2,29 @@ package migration
 
 import (
 	"fmt"
+	"sync"
 
 	"github.com/natenho/go-jira"
 )
 
 func (s *migrator) migrateComments(sourceIssue *jira.Issue, targetIssue *jira.Issue) chan error {
+	wg := &sync.WaitGroup{}
+
 	errChan := make(chan error, len(sourceIssue.Fields.Attachments))
+	defer close(errChan)
 
 	for _, item := range sourceIssue.Fields.Comments.Comments {
 		item.Body = fmt.Sprintf("_On %s [~accountid:%s] wrote:_\n\n%s", item.Created, item.Author.AccountID, item.Body)
 		_, response, err := s.targetClient.Issue.AddComment(targetIssue.ID, item)
 		if err != nil {
-			errChan <- parseResponseError("AddComment", response, err)
+			wg.Add(1)
+			go func() {
+				errChan <- parseResponseError("AddComment", response, err)
+				wg.Done()
+			}()
 		}
 	}
-	close(errChan)
+
+	wg.Wait()
 	return errChan
 }
